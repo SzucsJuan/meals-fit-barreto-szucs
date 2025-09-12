c0022a9d4b86a2246dd04a2c1d83ab64ff65cdc9
IanBarreto332 <ianbarreto99@gmail.com>
2025-09-12 19:10:48 -0300

    Version 0.3.3: Saved changes

--- backend/laravel/database/seeders/DatabaseSeeder.php
+++ backend/laravel/database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
-        $this->call(IngredientSeeder::class);
         $this->call(RecipeSeeder::class);
+        $this->call(IngredientSeeder::class);
         $this->call(MealLogSeeder::class);
     }
 }
