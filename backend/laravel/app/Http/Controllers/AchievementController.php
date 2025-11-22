9a738c618b731080b718fa853931491fa387d747
Juan Agustin Szucs <juanszucs@hotmail.com>
2025-11-22 01:30:59 -0300

    Fix page.tsx Achievements

--- backend/laravel/app/Http/Controllers/AchievementController.php
+++ backend/laravel/app/Http/Controllers/AchievementController.php
@@ -2,13 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Models\Achievement;
 use Illuminate\Http\Request;
 
 class AchievementController extends Controller
 {
+    /**
+     * Lógica para los achievements
+     */
     public function me(Request $request)
     {
-        $achievements = $request->user()->achievements;
+        $user = $request->user();
+
+        $achievements = Achievement::query()
+            ->leftJoin('user_achievements as ua', function ($join) use ($user) {
+                $join->on('ua.achievement_id', '=', 'achievements.id')
+                    ->where('ua.user_id', '=', $user->id);
+            })
+            ->select(
+                'achievements.id',
+                'achievements.name',
+                'achievements.description',
+                'achievements.created_at',
+                'achievements.updated_at',
+                'ua.awarded_at'
+            )
+            ->orderBy('achievements.id')
+            ->get();
+
+
         return response()->json($achievements);
     }
 }
