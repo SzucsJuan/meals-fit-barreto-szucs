99a3c10559052f8991651072bbb616de96f06fb2
Juan Agustin Szucs <juanszucs@hotmail.com>
2025-12-02 00:44:36 -0300

    Update verify

--- backend/laravel/app/Http/Middleware/VerifyCsrfToken.php
+++ backend/laravel/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,12 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
+        'login',
+        'logout',
+        'register',
+        'api/login',
+        'api/logout',
+        'api/register',
         'api/*',
     ];
 }
